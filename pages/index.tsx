4c398196c602d2425274d58924444c5dea83a4a0
Alain Iglesias <alain.iherrera@gmail.com>
2021-06-15 23:04:21 -0500

    feat(materialUI): add & configure material UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 /**
  * Home Page of the Application
  * @return {JSX.Element}
  */
 export default function Home(): JSX.Element {
-  return <> Winter is comming! </>;
+  return (
+    <>
+      <Typography variant="h1"> Winter is comming! </Typography>
+
+      <Button variant="contained" color="primary">
+        Burn them all!
+      </Button>
+    </>
+  );
 }
